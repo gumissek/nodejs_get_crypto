f584553fe465136402b0e01e3d71019637c2f7f8
piotrek <piotrek@ubuntu.com>
2025-03-26 09:10:23 +0100

    add get symbol

---
+++ index.js
@@ -0,0 +1,3 @@
+// zrobic apke ktora pobiera ze strony kurs kryto jaki chcemy
+// pierw wpisuje nazwe krypto jesli jej nie zna , klika ok i po tym uzupelnia mu inputa wyzej z symbolem https://www.npmjs.com/package/crypto-symbol
+// jesli zna symbol klika ok i dostaje kurs etc https://api.blockchain.com/v3/?javascript#gettickerbysymbol
